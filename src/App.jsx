93ab7a972b1fb196600d1410805c4b7a2e034e32
franc <fmenendezwork@gmail.com>
2023-08-06 22:37:38 -0700

    router implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,56 @@
-import { useState, useEffect, useRef } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
-import { DataContext } from "./context/DataContext";
-import { SearchContext } from "./context/SearchContext";
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [message, setMessage] = useState("Search for Music!");
-  const searchInput = useRef("");
+  let [search, setSearch] = useState("");
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState([]);
 
-  const API_URL = "https://itunes.apple.com/search?term"; //TERM
+  const API_URL = "https://itunes.apple.com/search?term=";
 
-  //PASS FUNCTION DOWN TO SEARCHBAR. setSearch = state setter function
+  useEffect(() => {
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`;
+        const response = await fetch(API_URL + search);
+        const resData = await response.json();
+        if (resData.results.length > 0) {
+          return setData(resData.results);
+        } else {
+          return setMessage("Not Found");
+        }
+      };
+      fetchData();
+    }
+  }, [search]);
+
   const handleSearch = (e, term) => {
-    e.preventDefault(); //PREVENTS FORM FROM REFRESHING PAGE AUTOMATICALLY. REFRESH COULD LOSE STATE VALUE
-    const fetchData = async () => {
-      document.title = `${term} Music`;
-      const response = await fetch(API_URL + term);
-      const resData = await response.json();
-      if (resData.results.length) {
-        return setData(resData.results);
-      } else {
-        return setMessage("Not found!");
-      }
-    };
-    fetchData();
+    e.preventDefault();
+    setSearch(term);
   };
 
-  //DATA SENT TO GALLERY THROUGH CONTEXT "CONTAINER"
-  //VALUE = SPECIAL PROP FOR CONTEXT, REMOVE DATA PROP FROM GALLERY
   return (
-    <div className="App">
-      <SearchContext.Provider
-        value={{
-          term: searchInput,
-          handleSearch: handleSearch,
-        }}
-      >
-        <SearchBar />
-      </SearchContext.Provider>
-      \{message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+    <div>
+      {message}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Fragment>
+                <SearchBar handleSearch={handleSearch} />
+                <Gallery data={data} />
+              </Fragment>
+            }
+          />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function GalleryItem(props) {
   const [viewDetails, setViewDetails] = useState(false); //SET TO FALSE TO TOGGLE WITH ONCLICK
@@ -31,16 +32,26 @@ export default function GalleryItem(props) {
     );
   };
 
-  const detailedView = () => {
+  const detailView = () => {
     return (
       <div style={detailStyle}>
-        <h2>{props.song.trackName}</h2>
-        <h3>{props.song.collectionName}</h3>
-        <h4>{props.song.primaryGenreName}</h4>
-        <h4>{props.song.releaseDate}</h4>
+        <h2>{props.item.trackName}</h2>
+        <h3>
+          <Link to={`/artist/${props.item.artistId}`}>
+            {props.item.artistName}
+          </Link>
+        </h3>
+        <h3>
+          <Link to={`/album/${props.item.collectionId}`}>
+            {props.item.collectionName}
+          </Link>
+        </h3>
+        <h4>{props.item.primaryGenreName}</h4>
+        <h4>{props.item.releaseDate}</h4>
       </div>
     );
   };
+
   //TERNARY SHOWS SIMPLE VIEW WHEN VIEW IS FALSE
   return (
     <div
